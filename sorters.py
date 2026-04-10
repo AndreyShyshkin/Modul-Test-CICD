83fcbf882d5233588863d7744d6c48a2184b7136
AndreyShyshkin <shyshkinandrey06@gmail.com>
2026-04-10 13:14:03 +0300

    feat: add sorting method by_area to CountrySorter for area-based sorting

--- sorters.py
+++ sorters.py
@@ -5,3 +5,5 @@ class CountrySorter:
     def __init__(self, countries: List[Country]):
         self._countries = countries
 
+    def by_area(self) -> List[Country]:
+        return sorted(self._countries, key=lambda c: c.area, reverse=True)
